546f54f1f282204f0577115ef024337323ea1694
Gadekar1116 <siddharthsgadekarbtech24@rvu.edu.in>
2024-11-06 18:35:38 +0530

    Update streamlitapp2.py

--- streamlitapp2.py
+++ streamlitapp2.py
@@ -12,15 +12,14 @@ st.title("Interactive Visualizations with Plotly and Streamlit")
 
 # --- Input for Author Information ---
 st.sidebar.header("Visualization skill workshop - Plotly")
-name = st.sidebar.text_input("Siddharth S Gadekar")
-usn = st.sidebar.text_input("Semetser-I Btech")
-instructor_name = st.sidebar.text_input("Prof. Ashwini Mathur _ SOCSE")
+name = st.sidebar.text_input("Enter your name","Siddharth S Gadekar")
+usn = st.sidebar.text_input("Enter your USN","Semetser-I Btech")
+instructor_name = st.sidebar.text_input(Enter INstructor's name","Prof. Ashwini Mathur _ SOCSE")
 
 
 # Display author information if provided
 if name and usn and instructor_name:
-    st.markdown
-    (
+    st.markdown(
         f"<h5 style='color: teal;'>Created by:</h5>"
         f"<p style='color: white;'>{name} (USN: {usn})</p>"
         f"<p style='color: white;'>Instructor: {instructor_name}</p>",
@@ -52,10 +51,10 @@ st.plotly_chart(fig2)  # Display the chart in Streamlit
 st.subheader("Task 3: Histogram Chart - Average Tip by Day")
 # Bar Chart: Average Tip by Day with color for each day
 fig3 = px.histogram(
-    tips, x='day', color='day',
-    title='Average Tip by Day',
-    labels={'tip': 'Average Tip ($)', 'day': 'Day of the Week'},
-    template='plotly_white'
+    tips, x='tip', color='sex',
+    title='Distribution of Tips (Colored by Gender)',
+    labels={'tip': 'Tip ($)', 'sex': 'Gender'},
+    template='plotly_white', # Clean and bright look
 )
 st.plotly_chart(fig3)  # Display the chart in Streamlit
 
